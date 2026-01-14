3a90b24490990060b4d1650b1edc8747f277796c
Paweł Żurowski <zurowski.pawel@gmail.com>
2026-01-14 13:16:58 +0100

    Expand `math` library with new functions (`acos`, `atan2`, `cos`, `log`, `rad`, `sin`, `random`) and constants (`pi`, `huge`).

--- src/stdlib/math.ts
+++ src/stdlib/math.ts
@@ -6,25 +6,24 @@ import {
   TableValue,
   Value,
 } from '@src/interpreter/types';
-import { countRegularArguments, requestNumber } from '@src/interpreter/utils';
-
-function getNumberOrError(args: Value[]): number {
-  const n = requestNumber(args);
-  return n.number;
-}
+import {
+  countRegularArguments,
+  requestNumber,
+  requestNumberOrNil,
+} from '@src/interpreter/utils';
 
 function abs(args: Value[]): Value[] {
-  const n = getNumberOrError(args);
+  const n = requestNumber(args).number;
   return [NumberValue.from(Math.abs(n))];
 }
 
 function ceil(args: Value[]): Value[] {
-  const n = getNumberOrError(args);
+  const n = requestNumber(args).number;
   return [NumberValue.from(Math.ceil(n))];
 }
 
 function floor(args: Value[]): Value[] {
-  const n = getNumberOrError(args);
+  const n = requestNumber(args).number;
   return [NumberValue.from(Math.floor(n))];
 }
 
@@ -55,16 +54,76 @@ function min(args: Value[]): Value[] {
 }
 
 function sqrt(args: Value[]): Value[] {
-  const n = getNumberOrError(args);
+  const n = requestNumber(args).number;
   return [NumberValue.from(Math.sqrt(n))];
+}
+
+function acos(args: Value[]): Value[] {
+  const n = requestNumber(args).number;
+  return [NumberValue.from(Math.acos(n))];
+}
+
+function atan2(args: Value[]): Value[] {
+  const y = requestNumber(args, 0).number;
+  const x = requestNumber(args, 1).number;
+  return [NumberValue.from(Math.atan2(y, x))];
+}
+
+function cos(args: Value[]): Value[] {
+  const n = requestNumber(args).number;
+  return [NumberValue.from(Math.cos(n))];
+}
+
+function log(args: Value[]): Value[] {
+  const n = requestNumber(args).number;
+  return [NumberValue.from(Math.log(n))];
+}
+
+function rad(args: Value[]): Value[] {
+  const n = requestNumber(args).number;
+  return [NumberValue.from((n * Math.PI) / 180)];
+}
+
+function sin(args: Value[]): Value[] {
+  const n = requestNumber(args).number;
+  return [NumberValue.from(Math.sin(n))];
+}
+
+function random(args: Value[]): Value[] {
+  const mValue = requestNumberOrNil(args, 0);
+  const nValue = requestNumberOrNil(args, 1);
+
+  if (mValue instanceof NilValue) {
+    return [NumberValue.from(Math.random())];
+  }
+
+  if (nValue instanceof NilValue) {
+    return [NumberValue.from(Math.floor(Math.random() * mValue.number) + 1)];
+  }
+
+  return [
+    NumberValue.from(
+      Math.floor(Math.random() * (nValue.number - mValue.number + 1)) +
+        mValue.number
+    ),
+  ];
 }
 
 const functions = new TableValue();
 functions.set(StringValue.from('abs'), ExtFunction.of(abs));
+functions.set(StringValue.from('acos'), ExtFunction.of(acos));
+functions.set(StringValue.from('atan2'), ExtFunction.of(atan2));
 functions.set(StringValue.from('ceil'), ExtFunction.of(ceil));
+functions.set(StringValue.from('cos'), ExtFunction.of(cos));
 functions.set(StringValue.from('floor'), ExtFunction.of(floor));
+functions.set(StringValue.from('huge'), NumberValue.from(Infinity));
+functions.set(StringValue.from('log'), ExtFunction.of(log));
 functions.set(StringValue.from('max'), ExtFunction.of(max));
 functions.set(StringValue.from('min'), ExtFunction.of(min));
+functions.set(StringValue.from('pi'), NumberValue.from(Math.PI));
+functions.set(StringValue.from('rad'), ExtFunction.of(rad));
+functions.set(StringValue.from('random'), ExtFunction.of(random));
+functions.set(StringValue.from('sin'), ExtFunction.of(sin));
 functions.set(StringValue.from('sqrt'), ExtFunction.of(sqrt));
 const mathStdLib = new TableValue();
 mathStdLib.set(StringValue.from('math'), functions);
--- tests/stdlib/math.test.ts
+++ tests/stdlib/math.test.ts
@@ -10,6 +10,17 @@ test('math', () => {
     max = math.max(5,3,1,2,4)
     min = math.min(5,3,1,2,4)
     sqrt = math.sqrt(25)
+    acos = math.acos(0.5)
+    atan2 = math.atan2(1, 2)
+    cos = math.cos(math.pi)
+    huge = math.huge
+    log = math.log(10)
+    pi = math.pi
+    rad = math.rad(180)
+    sin = math.sin(math.pi / 2)
+    random = math.random()
+    random1 = math.random(1)
+    random2 = math.random(10, 20)
     `;
   const vm = new VMBuilder().witStdLib().build();
   const result = vm.executeOnce(lua);
@@ -20,4 +31,23 @@ test('math', () => {
   expectToBeNumber(result.globalVar('max'), 5);
   expectToBeNumber(result.globalVar('min'), 1);
   expectToBeNumber(result.globalVar('sqrt'), 5);
+
+  expectToBeNumber(result.globalVar('acos'), Math.acos(0.5));
+  expectToBeNumber(result.globalVar('atan2'), Math.atan2(1, 2));
+  expectToBeNumber(result.globalVar('cos'), Math.cos(Math.PI));
+  expectToBeNumber(result.globalVar('huge'), Infinity);
+  expectToBeNumber(result.globalVar('log'), Math.log(10));
+  expectToBeNumber(result.globalVar('pi'), Math.PI);
+  expectToBeNumber(result.globalVar('rad'), (180 * Math.PI) / 180);
+  expectToBeNumber(result.globalVar('sin'), Math.sin(Math.PI / 2));
+
+  const rnd = (result.globalVar('random') as any).number;
+  expect(rnd).toBeGreaterThanOrEqual(0);
+  expect(rnd).toBeLessThan(1);
+
+  expectToBeNumber(result.globalVar('random1'), 1); // math.random(1) should be 1
+  const rnd2 = (result.globalVar('random2') as any).number;
+  expect(rnd2).toBeGreaterThanOrEqual(10);
+  expect(rnd2).toBeLessThanOrEqual(20);
+  expect(rnd2 % 1).toBe(0);
 });
